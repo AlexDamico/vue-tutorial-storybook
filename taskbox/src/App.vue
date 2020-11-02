a09ea644ced91f92da1579e0e01bd611b4c28ded
Alex D'Amico <AlexDamico@users.noreply.github.com>
2020-11-02 12:33:51 -0500

    broken(project): Wrap TastList in a screen component

--- taskbox/src/App.vue
+++ taskbox/src/App.vue
@@ -1,23 +1,21 @@
 <!--src/App.vue-->
 <template>
   <div id="app">
-    <task-list />
+    <inbox-screen />
   </div>
 </template>
 
 <script>
   import store from './store';
-  import TaskList from './components/TaskList.vue';
-
+  import InboxScreen from './components/InboxScreen.vue';
   export default {
     name: 'app',
     store,
     components: {
-      TaskList,
+      InboxScreen,
     },
   };
 </script>
-
 <style>
   @import './index.css';
 </style>
